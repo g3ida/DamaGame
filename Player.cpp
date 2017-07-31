f23f7c03089c030f410bbbe4db0fafda1acae617
Mohamed Alaa Eddine Gaida <m.gaida@yahoo.com>
2017-07-31 11:47:07 +0200

    Delete Player.cpp

--- Player.cpp
+++
@@ -1,8 +0,0 @@
-#include "Player.h"
-#include "Log.h"
-
-std::pair<short int, short int>
-Player::makeMove(Damier& dam, std::vector<std::pair<short int, short int>>& moves)
-{
-    return moves[0];
-}
